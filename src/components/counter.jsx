e70ed871a608ffb713f0ead3add6415b615701db
Brett Austin Eastman <brett.austin.eastman@gmail.com>
2022-12-28 17:51:12 -0800

    Through most of #11 - using constructor and super with bind to bind this to a function

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,19 +6,34 @@ class Counter extends React.Component {
     tags: []
   };
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+  // **FYI - this is the longer, classic way, using bind. For arrow function version, see next commit
 
-    return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
+  constructor() { // Because we called the constructor of the child class, Counter, first we have to call the constructor of the parent class, React.Component, using the "super" keyword. In other words, we need to have super in there in order to have 'this' be defined as the Counter
+    super();
+    this.handleIncrement = this.handleIncrement.bind(this); // the bind method will make it to where, when we use the handleIncrement method, it will return a new instance of the handleIncrement function and in that new instance, this will always refer to the current object, rather than the window object.
   }
 
-  render() { // in JS, with the && operator, if both values are truthy, it will return the 2nd one
+  handleIncrement() {
+    console.log('clicked!', this)
+  }
+
+  render() {
     return (
     <div>
-      {this.state.tags.length === 0 && 'Please create new tags'}
-      {this.renderTags()}
+      <span className={this.getBadgeClasses()}>{this.formatCount()}</span><button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
     </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += (this.state.count === 0) ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? 'Zero' : count;
   }
 }
 
