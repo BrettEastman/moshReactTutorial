8f835fcbe1490a79dc20c78600bb83f161341edf
Brett Austin Eastman <brett.austin.eastman@gmail.com>
2022-12-29 20:43:16 -0800

    Up to Composing Components #2

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,17 +5,21 @@ class Counter extends React.Component {
     count: 0,
   };
 
-  handleIncrement = product => {
-    console.log(product);
-    this.setState({ count: this.state.count + 1});
-  }
+  handleIncrement = () => {
+    this.setState({count: this.state.count + 1});
+  };
 
   render() {
     return ( // use a wrapper arrow function with handleIncrement to be able to add an argument to it. For this example, we are hard coding "id: 1", but it could be the results from mapping over data.
-    <div>
-      <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-      <button onClick={() => this.handleIncrement({ id: 1 })} className="btn btn-secondary btn-sm">Increment</button>
-    </div>
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </div>
     );
   }
 
